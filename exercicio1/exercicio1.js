e84ee9bd6b3496f77e60d7d3532ba40c28a1cbf0
Diegopkj-d <cpdtecnico01@gmail.com>
2023-03-05 22:29:33 -0300

    correção na logica

---
+++ exercicio1/exercicio1.js
@@ -0,0 +1,8 @@
+let base=prompt(" Digite o valor da base : ");
+let altura=prompt("Digite o valor da altura :");
+
+function calculaArea (base,altura) {
+    let area =  base*altura;
+    return area;
+};
+calculaArea()
