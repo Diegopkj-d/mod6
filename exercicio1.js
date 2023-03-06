1304bcaf3bf81631dac8980213302777a6d2a311
Diegopkjd-d <125473084+Diegopkj-d@users.noreply.github.com>
2023-03-05 22:30:32 -0300

    Delete exercicio1.js

--- exercicio1.js
+++
@@ -1,7 +0,0 @@
-let base=prompt(" Digite o valor da base : ");
-let altura=prompt("Digite o valor da altura :");
-
-function calculaArea () {
-    alert (" A area é :" + base * altura)
-};
-calculaArea()
