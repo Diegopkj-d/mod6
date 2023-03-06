0284e88a652c96323d9d1d10deb5f8f162eb1ce6
Diegopkj-d <cpdtecnico01@gmail.com>
2023-03-05 21:24:20 -0300

    first commit

---
+++ exercicio1.js
@@ -0,0 +1,7 @@
+let base=prompt(" Digite o valor da base : ");
+let altura=prompt("Digite o valor da altura :");
+
+function calculaArea () {
+    alert (" A area é :" + base * altura)
+};
+calculaArea()
